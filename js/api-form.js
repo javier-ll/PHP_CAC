f1a343b91eddacbd0f92d94e6a93fbb601d1ecc9
javier-ll <llanojavier97@gmail.com>
2024-05-25 12:40:10 -0300

    Método GET completado

--- js/api-form.js
+++ js/api-form.js
@@ -1,15 +1,45 @@
 // Uso de API SheetDBio
-var form = document.getElementById('sheetdb-form');
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  fetch(form.action, {
-      method : "POST",
-      body: new FormData(document.getElementById("sheetdb-form")),
-  }).then(
-      response => response.json()
-  ).then((html) => {
-    // you can put any JS code here
-    alert('Listo! Gracias por comunicarte con nosotros.')
-    location.reload(); 
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('sheetdb-form');
+  
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    
+    const submitButton = form.querySelector('button[type="submit"]');
+    submitButton.disabled = true; // Deshabilitar el botón de envío
+
+    await fetch(form.action, {
+      method: "POST",
+      body: new FormData(form),
+    }).then(response => response.json())
+      .then(() => {
+        // Mostrar mensaje de éxito
+        alert('Listo! Gracias por comunicarte con nosotros.');
+        location.reload(); // Recargar la página
+      });
+
+    submitButton.disabled = false; // Habilitar el botón de envío nuevamente
   });
+});
+
+// Método GET
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('https://sheetdb.io/api/v1/7ojzs763c0lud')
+  .then(response => response.json())
+  .then(data => {
+      const registrosBody = document.getElementById('registros-body');
+      data.forEach(registro => {
+          const row = document.createElement('tr');
+          row.innerHTML = `
+              <td>${registro.empresa}</td>
+              <td>${registro.representante}</td>
+              <td>${registro.email}</td>
+              <td>${registro.ciudad}</td>
+              <td>${registro.telefono}</td>
+              <td>${registro.consulta}</td>
+          `;
+          registrosBody.appendChild(row);
+      });
+  })
+  .catch(error => console.error('Error al obtener los registros:', error));
 });
